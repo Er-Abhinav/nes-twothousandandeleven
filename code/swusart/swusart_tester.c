d7df4af1a445d6a082e50a631c654526dca33164
pocketbroadcast <pocketbroadcast@20439a3e-a577-ede2-054a-ed5457ea85a5>
2011-12-07 14:40:22 +0000

    adding driver skeleton for swusart (not finished yet)

--- code/swusart/swusart_tester.c
+++ code/swusart/swusart_tester.c
@@ -1,5 +1,40 @@
+#include <avr/io.h>
+#include <stdio.h>
+
 #include "swusart.h"
 
-void main (void) {
-    // insert tester for swusart here
+void startBitDetected();
+void transmittedData(uint8_t data);
+void corruptedData(uint8_t dataSent, uint8_t dataReceived);
+
+int main (void) {
+
+  DDRC = 0xFF;
+  PORTC = 0x0F;
+
+  // initialize the usart
+  SWUSART_initialize (MODE_8N1,
+                      9600,
+                      startBitDetected,
+		              transmittedData,
+                      corruptedData);
+
+  sei();
+
+  //send_byte('a');
+
+  while(1) {
+  }
+
+  return 0;
+} // end of main
+
+void startBitDetected() {
+   PORTC = 0xF0;
+} // end of startBitDetected
+
+void transmittedData(uint8_t data) {
+}
+
+void corruptedData(uint8_t dataSent, uint8_t dataReceived) {
 }
