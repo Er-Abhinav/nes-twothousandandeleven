e15a794887e9125d5e0a3b9de800778ad99ba054
nickma82 <nickma82@20439a3e-a577-ede2-054a-ed5457ea85a5>
2011-12-08 21:30:33 +0000

    time hinzugefügt und bus protokoll begonnen

--- src/library/eselib/include/ulfBus.h
+++ src/library/eselib/include/ulfBus.h
@@ -1,3 +1,33 @@
+/*======================================================================*/
+/* Project:						*/
+/* MCU      Atmel AVR megaX		*/
+/**@file	ulfBus.h
+   @author 	Robert, Alex, Nick
+   @version 0.1
+   @date 	2011/12/05			*/
+/*======================================================================*/
+#ifndef __ULFBUS_H__
+#define __ULFBUS_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <avr/io.h>
+
+typedef enum {
+	NEW,
+	RUN,
+	FINISHED,
+	QUEUED,
+	DEAD
+} msg_state;
+
+typedef enum {
+	ATTACH,
+	DETACH
+} attach_detach_t;
+
 
 typedef enum {
 	UNKNOWN = 0,
@@ -10,18 +40,22 @@ typedef enum {
  * Start/Sets timer
  * Checks the bus
  */
-l1_bus_status getBus(uint16_t arbitration);
+l1_bus_status l1GetBus(uint16_t arbitration_data);
 /**
- * @pre Bus has to be Claimed
+ * @pre Bus has to be CLAIMED
  */
-l1_bus_status pushSendBus(uint8_t len, uint8_t *payload);
+l1_bus_status l1PushSendBus(uint8_t len, uint8_t *payload);
+
 /**
  * Checks if further data are queued
  * pushes data to the HW-UART
  *
  * CHECK if message to send is a time message
  */
-void callbackMsgReceived(void);
+void l1CallbackMsgReceived(void);
+
+uint8_t l1HandlePreSendCallback(attach_detach_t action, uint8_t msgType, \
+		void* callback,);
 
 
 typedef struct {
@@ -40,7 +74,15 @@ typedef struct {
 	l2msgHeader_t* header;
 	uint8_t* data;
 	uint8_t* crc;
+	msg_state state;
 } l2msg_t;
 
-uint8_t attachL2Send(l2msg_t *msg, uint8_t priority);
-uint8_t initL2(void);
+uint8_t l2init(void);
+uint8_t l2attachSend(l2msg_t *msg, uint8_t priority);
+uint8_t _l2l1AttachMiddleware(l2msg_t *msg, uint8_t priority);
+
+
+#ifdef __cplusplus
+}
+#endif
+#endif __ULFBUS_H__
