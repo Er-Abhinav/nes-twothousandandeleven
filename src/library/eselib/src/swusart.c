45f0fd494c2ae3a9528d2ace8bcbd28d2c67a117
nickma82@gmail.com <nickma82@gmail.com@20439a3e-a577-ede2-054a-ed5457ea85a5>
2011-12-19 17:29:16 +0000

    werken am SW-uart

--- src/library/eselib/src/swusart.c
+++ src/library/eselib/src/swusart.c
@@ -1,64 +1,32 @@
 #include <stdio.h>
 #include "swusart.h"
-#include "single_wire_UART.h"
 
-// internal
-#define SWUSART_STATE_UNINITIALIZED  0
-#define SWUSART_STATE_IDLE           1
-#define SWUSART_STATE_WRITE_STARTBIT 2
-#define SWUSART_STATE_WRITE_DATABIT  3
-#define SWUSART_STATE_WRITE_STOPBIT  4
+
 
 /* Communication parameters. */
 static volatile uint8_t   UART_Tx_data;     //!< Byte holding data being transmitted.
 static volatile uint8_t   UART_Rx_data;     //!< Byte holding data being received.
-static volatile uint8_t   UART_Tx_buffer;   //!< Transmission buffer.
-static volatile uint8_t   UART_Rx_buffer;   //!< Reception buffer.
 
-enum _swUart_state {
-	UNINITIALIZED = -1,
-	IDLE = 0,
-	WRITE_STARTBIT,
-	WRITE_DATA,
-	WRITE_STOP,
-	FINISH
-};
 
-enum _swUart_finish_states {
-	UNKNOWN = -1,
-	BUSY,
-	GOT_BUS,
-	ARBITRATION_LOST
-};
 
-volatile uint8_t SW_UART_status = 0;
-volatile uint8_t UART_counter;
+typedef struct _sw_uart_s {
+	_sw_uart_state_t state;
+	uint8_t bitCount;
+} _sw_uart_t;
+volatile _sw_uart_t _sw_uart = {UNINITIALIZED, 0};
 
-/*! \brief  Enable the UART.
- *
- *  This function initialize the timer and buffers,
- *  and enables the external interrupt to sense
- *  for incoming data. It is important to run this
- *  function before calling any of the other UART
- *  functions. Received data will automatically be
- *  put in the RX buffer.
- *
- *  \note   The UART can be stopped by disabling the
- *          timer and external interrupt.
- */
-void SW_UART_Enable(void)
-{
+
+int8_t SW_UART_Enable(void){
   //Tri-state communication pin.
   INITIALIZE_UART_PIN();
 
-  SW_UART_status = 0x00;
-  UART_counter = UART_STATE_IDLE;
   CLEAR_UART_TIMER_ON_COMPARE_MATCH();
-
-  //Set up interrupts
-  INITIALIZE_UART_EXTERNAL_INTERRUPT2(); //Set up the external interrupt to wait for incoming start bit on correct pin.
-  CLEAR_UART_EXTERNAL_INTERRUPT2_FLAG(); //Clear flag in case it is already set for some reason.
-  ENABLE_UART_EXTERNAL_INTERRUPT2();
+  if (_sw_uart.state  <= 0) {
+	  _sw_uart.state = ENABLED;
+  } else {
+	  return -1;
+  }
+  return 0;
 }
 
 
@@ -69,10 +37,9 @@ void SW_UART_Enable(void)
  */
 void SW_UART_Disable()
 {
-  SW_UART_status = 0x00;
   DISABLE_UART_TIMER_INTERRUPT();
-  DISABLE_UART_EXTERNAL_INTERRUPT();
   STOP_UART_TIMER();
+  _sw_uart.state = DISABLED;
 }
 
 /*! \brief  Transmit one byte.
@@ -87,36 +54,87 @@ void SW_UART_Disable()
  *
  *  \param  data  Data to be sent.
  */
-void SW_UART_Transmit(uint8_t data)
-{
-  SET_FLAG( SW_UART_status, SW_UART_TX_BUFFER_FULL );
-  UART_Tx_buffer = data;
+int8_t swUart_arbitrate(uint8_t data) {
+  UART_Tx_data = data;
+
+  if (_sw_uart.state != ENABLED)
+	  return BUSY;
 
   //Start transmission if no ongoing communication.
-  if( UART_counter == UART_STATE_IDLE )
+  if( _sw_uart.state == ENABLED )
   {
-	 PORTC = 0x01;
     //Copy byte from buffer and clear buffer full flag.
-    UART_Tx_data = UART_Tx_buffer;
-    CLEAR_FLAG( SW_UART_status, SW_UART_TX_BUFFER_FULL );
+    //CLEAR_FLAG( SW_UART_status, SW_UART_TX_BUFFER_FULL );
 
-    DISABLE_UART_EXTERNAL_INTERRUPT2();
     CLEAR_UART_PIN();                         //Write start bit.
-    UART_counter = TRANSMIT_FIRST_DATA;       //Update counter so the 1. data bit is the next bit to be transmitted.
+    _sw_uart.state = WRITE_STARTBIT;
+    _sw_uart.bitCount = 0;
     CLEAR_UART_TIMER();                       //Clear timer.
     SET_UART_TIMER_COMPARE_START_TRANSMIT();  //Set timer compare value.
     CLEAR_UART_TIMER_INTERRUPT_FLAG();        //Make sure timer interrupt flag is not set.
     ENABLE_UART_TIMER_INTERRUPT();
     START_UART_TIMER();
-    PORTC |= 0x02;
+    //@TODO polling bus enabling -> make timer INT, send_startbit??
   }
+  return 0;
 }
 
 
+/*! \brief  Timer compare interrupt service routine
+ *
+ *  This interrupt is triggered when the compare
+ *  register equals the timer. It increments the
+ *  counter and handles the transmission and
+ *  reception of each bit.
+ *
+ *  \note   The WAIT_ONE define has to be less than
+ *          the maximum time spent in this routine
+ *          so an interrupt is not missed.
+ */
+ISR(SW_UART_TIMER_COMPARE_INTERRUPT_VECTOR) {
+	SET_UART_TIMER_COMPARE_WAIT_ONE(); //Set timer compare value to trigger the ISR once every bit period.
 
+	//Sample bit by checking the value on the UART pin:
+	uint8_t bit_in = 0, bit_out = 0;
+	uint8_t last_bit_sent = 0;
 
+	switch(_sw_uart.state) {
+	case ENABLED:
+		break;
+	case WRITE_STARTBIT:
+		PORTA = ~(_sw_uart.bitCount++);
+		break;
+	case WRITE_DATA:
 
-enum _swUart_state _sw_uart_state = SWUSART_STATE_UNINITIALIZED;
+		if(bit_out) SET_UART_PIN();
+		else CLEAR_UART_PIN();
+		break;
+	case READ_DATA:
+		bit_in = (READ_UART_PIN()) ? 1 : 0;
+		if (_sw_uart.bitCount == 1) {
+			asm("nop;");
+		}
+
+		if (_sw_uart.bitCount == 7) {
+			last_bit_sent = UART_Tx_data & 0x01;
+		}
+
+		//If to receive data bit -> Copy received bit into Rx_data.
+	    if(_sw_uart.bitCount <= 7) {
+	      UART_Rx_data = ( UART_Rx_data >> 1 ); //Right shift RX_data so the new bit can be masked into the Rx_data byte.
+	      if(bit_in) UART_Rx_data |= 0x80;               //Set MSB of RX data if received bit == 1.
+	    }
+		break;
+	case WRITE_STOP:
+		break;
+	case FINISH:
+		break;
+	default:
+		break;
+	}
+
+}
+
 
 // events
 void(*onStartBitDetected)(void) = 0;
@@ -124,38 +142,16 @@ void(*onStartBitDetected)(void) = 0;
 // methods
 void SWUSART_initialize(uint8_t mode,
                         uint32_t baud,
-                        void(*startBitDetectedCallback)()
+                        void(*startBitDetectedCallback)(void)
                         ) {
-
     onStartBitDetected = startBitDetectedCallback;
-
-    //Set up interrupts
-    INITIALIZE_UART_EXTERNAL_INTERRUPT2();
-    CLEAR_UART_EXTERNAL_INTERRUPT2_FLAG();
-    ENABLE_UART_EXTERNAL_INTERRUPT2();
-
-    // initialize hw usart
-    _sw_uart_state = IDLE;
-} // end of HWUSART_initialize
-
-void SWUSART_writeByte (uint8_t data) {
-	SW_UART_Enable();
-    // trigger write for sw usart
-	SW_UART_Transmit(data);
-} // end of SWUSART_writeByte
-
-
-//-----------
-// internal -
-//-----------
-ISR(SW_UART_EXTERNAL_INTERRUPT2_VECTOR) {
-    onStartBitDetected();
+    _sw_uart.state = DISABLED;
 }
 
-//ISR(SW_UART_EXTERNAL_INTERRUPT_VECTOR) {
-//
-//    //Make sure bit is low.
-//    if(!READ_UART_PIN()) {
-//	    onStartBitDetected();
-//    } // end if
-//} // end of SWUSART_externalISR
+void SWUSART_writeByte (uint8_t data) {
+	if (SW_UART_Enable() < 0)
+		return;
+    // trigger write for sw usart
+	swUart_arbitrate(data);
+} // end of SWUSART_writeByte
+
